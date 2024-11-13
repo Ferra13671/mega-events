1cc11ac72a8cfa193d7bd48303d8a254d678799b
Ferra <ferra13671@gmail.com>
2024-11-13 16:27:01 +0300

    1.1.1

---
+++ src/main/java/com/ferra13671/MegaEvents/EventModifiers/EventPriority.java
@@ -0,0 +1,14 @@
+package com.ferra13671.MegaEvents.EventModifiers;
+
+/**
+ * @author Bebra_tyan(Blue_tyan)
+ * @version 1
+ */
+
+public interface EventPriority {
+    int HIGHEST = 300;
+    int HIGH = 150;
+    int DEFAULT = 0;
+    int LOW = -150;
+    int LOWEST = -350;
+}
