32f41492caf59178f35a09cc42a8fd2fdb5b118b
Ferra13671 <ferra13671@gmail.com>
2025-05-09 12:59:03 +0300

    1.3

--- src/main/java/com/ferra13671/MegaEvents/Base/EventBus.java
+++ src/main/java/com/ferra13671/MegaEvents/Base/EventBus.java
@@ -2,54 +2,80 @@ package com.ferra13671.MegaEvents.Base;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 /**
  * @author Ferra13671
- * @LastUpdate 1.1
+ * @LastUpdate 1.3
  */
 
+
 public class EventBus implements IEventBus {
-    private final HashMap<Object, List<Method>> registeredMap = new HashMap<>();
+    private final HashMap<Class<?>, List<MethodWithObject>> registeredMap = new HashMap<>();
 
     @Override
     public void register(Object object) {
-
-        registeredMap.put(object, Arrays.stream(object.getClass().getDeclaredMethods())
-                .filter(method -> method.isAnnotationPresent(EventSubscriber.class))
-                .sorted(Comparator.comparing(method -> method.getAnnotation(EventSubscriber.class).priority()))
-                .collect(Collectors.toList())
-        );
+        HashMap<Class<?>, List<MethodWithObject>> temp = new HashMap<>();
+        for (Method method : object.getClass().getDeclaredMethods()) {
+            if (method.isAnnotationPresent(EventSubscriber.class)) {
+                if (method.getParameterTypes().length > 0) {
+                    if (temp.containsKey(method.getParameterTypes()[0])) {
+                        temp.get(method.getParameterTypes()[0]).add(new MethodWithObject(object, method));
+                    } else {
+                        temp.put(method.getParameterTypes()[0], new ArrayList<>(Collections.singletonList(new MethodWithObject(object, method))));
+                    }
+                }
+            }
+        }
+        temp.forEach((_class, methods) -> {
+            if (registeredMap.containsKey(_class)) {
+                registeredMap.get(_class).addAll(methods);
+            } else {
+                registeredMap.put(_class, new ArrayList<>(methods));
+            }
+            registeredMap.get(_class).sort(Comparator.comparing(method -> method.method.getAnnotation(EventSubscriber.class).priority()));
+            Collections.reverse(registeredMap.get(_class));
+        });
     }
 
     @Override
     public void unregister(Object object) {
-
-        registeredMap.remove(object);
+        registeredMap.forEach((_class, methods) -> {
+            Method[] objectMethods = object.getClass().getDeclaredMethods();
+            methods.removeIf(method -> {
+                for (Method m : objectMethods)
+                    if (method.method.equals(m)) return true;
+                return false;
+            });
+        });
     }
 
     @Override
     public <T extends Event> void activate(T event) {
-        if (event.isCancelled()) return;
-
-        HashMap<Object, List<Method>> registeredMap = this.registeredMap;
-
         try {
-            registeredMap.forEach((object, methods) -> methods
-                    .forEach(method -> {
-                        if (method.getParameterTypes()[0] != event.getClass()) return;
-
+            registeredMap.forEach((_class, methods) -> {
+                if (_class.equals(event.getClass())) {
+                    methods.forEach(method -> {
                         try {
-                            method.invoke(object, event);
-                        } catch (IllegalAccessException | InvocationTargetException e) {
+                            method.method.invoke(method.object, event);
+                        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
                             e.printStackTrace();
                         }
-                    })
-            );
-        } catch (Exception ignored) {}
+                    });
+                }
+            });
+        } catch (Exception e) {
+            if (!(e instanceof ConcurrentModificationException)) e.printStackTrace();
+        }
+    }
+
+    private static class MethodWithObject {
+        public final Object object;
+        public final Method method;
+
+        public MethodWithObject(Object object, Method method) {
+            this.object = object;
+            this.method = method;
+        }
     }
 }
